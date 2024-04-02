7f684c78417510aaa210be85f56c0bf131099b19
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 23:13:55 +0200

    add provider

---
+++ src/Provider.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+
+interface ProviderProps {
+    children: React.ReactNode
+}
+
+
+export const queryClient = new QueryClient()
+export function Provider({ children }: ProviderProps) {
+    return (
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+}
