bae9c659e55194f983718aea0ccead6f7bef32a5
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 22:52:36 +0200

    fix component Layout

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,11 +1,15 @@
 import styles from './Layout.module.css';
 import Item from '../../components/Item/Item.tsx';
+import CreateForm from "../../components/Forms/CreateForm.tsx";
+import UpdateForm from "../../components/Forms/UpdateForm.tsx";
 
 const Layout = () => {
   return (
     <section className={styles.wrapper}>
       <div className='container'>
         <div className={styles.items}>
+          <CreateForm/>
+          <UpdateForm/>
           <Item />
           <Item />
           <Item />
