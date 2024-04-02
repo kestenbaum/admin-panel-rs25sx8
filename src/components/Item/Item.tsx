1a025f262aef5c8756bf24c6f80f2de776dc93c1
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 23:14:14 +0200

    add types and props for Item

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,10 +1,11 @@
-import styles from './Item.module.css';
+import {FC} from "react";
 import Button from '../UI/Button/Button.tsx';
+import styles from './Item.module.css';
 
-const Item = () => {
+const Item:FC<Item> = ({title}) => {
   return (
     <section className={styles.wrapper}>
-      <span className={styles.title}>Title</span>
+      <span className={styles.title}>{title}</span>
       <Button>Open</Button>
     </section>
   );
