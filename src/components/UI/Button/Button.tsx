f15f94e0cfe1346c36ea34ab84b96ac369c56c44
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 20:15:06 +0200

    update component Button: .prettierrc

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,16 +1,16 @@
-import React, {FC, ReactNode} from "react";
-import styles from "./Button.module.css"
+import React, { FC, ReactNode } from 'react';
+import styles from './Button.module.css';
 
-interface Button extends React.ButtonHTMLAttributes<HTMLButtonElement>{
-    children: ReactNode | string
+interface Button extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode | string;
 }
 
-const Button:FC<Button> = ({children, ...props}) => {
-    return (
-        <button className={styles.btn} {...props}>
-            {children}
-        </button>
-    );
+const Button: FC<Button> = ({ children, ...props }) => {
+  return (
+    <button className={styles.btn} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
