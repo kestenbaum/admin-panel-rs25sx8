6baf08897a99da17a80cb7b505a0ae71387e033c
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 21:05:42 +0200

    update component Header: add sidebar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,24 @@
 import styles from './Header.module.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import {useState} from "react";
+import Sidebar from "../Sidebar/Sidebar.tsx";
+
 
 const Header = () => {
+  const [isActive, setActive] = useState<boolean>(false)
+
   return (
     <header className={styles.header}>
       <div className='container'>
         <div className={styles.wrapper}>
           <span className={styles.logo}>Admin-Panel</span>
-          <span className={styles.logo}>
-            <GiHamburgerMenu />
-          </span>
+          <Sidebar active={isActive} setActive={setActive}/>
+            <div
+                className={styles.menu}
+                onClick={() => setActive(!isActive)}
+            >
+              <GiHamburgerMenu />
+            </div>
         </div>
       </div>
     </header>
