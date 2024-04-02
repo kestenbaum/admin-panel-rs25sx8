bcc710ef5114fbd239bc3de088424a767af00673
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 19:48:44 +0200

    create component: Layout

---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import styles from './Layout.module.css';
+
+const Layout = () => {
+  return (
+    <section className={styles.wrapper}>
+      <div className='container'>
+        <div>Main</div>
+      </div>
+    </section>
+  );
+};
+
+export default Layout;
