f8673fdf98030a9f363776c84aad604f6d8cbe57
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 23:14:51 +0200

    add request<get> for layout

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,20 +1,28 @@
-import styles from './Layout.module.css';
 import Item from '../../components/Item/Item.tsx';
 import CreateForm from "../../components/Forms/CreateForm.tsx";
 import UpdateForm from "../../components/Forms/UpdateForm.tsx";
+import styles from './Layout.module.css';
+import {useQuery} from "react-query";
+import {getItems} from "../../api/request/items.ts";
 
 const Layout = () => {
+  const {data, isLoading} = useQuery({
+   queryKey: ["items"],
+   queryFn: () => getItems().then(responce => responce.data)
+  })
+
   return (
     <section className={styles.wrapper}>
       <div className='container'>
         <div className={styles.items}>
           <CreateForm/>
           <UpdateForm/>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
+          {!isLoading
+              ?
+              data?.data.map(item => <Item key={item._id} title={item.title}/>)
+            :
+            <div>Loader</div>
+          }
         </div>
       </div>
     </section>
