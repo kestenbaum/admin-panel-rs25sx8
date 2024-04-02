7ef669de481e8d0d46eb3f3f896c01d87e72ca0e
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-02 21:05:17 +0200

    create component Sidebar

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+import Button from "../UI/Button/Button.tsx";
+import {FC} from "react";
+
+interface SidebarProps {
+    active: boolean,
+    setActive: (active:boolean) => void
+}
+
+const Sidebar:FC<SidebarProps> = ({active, setActive}) => {
+    return (
+        <div className={active ? "sidebar" : "sidebar active"}>
+            <Button onClick={() => setActive(false)}>Create</Button>
+            <Button onClick={() => setActive(false)}>LogOut</Button>
+        </div>
+    );
+};
+
+export default Sidebar;
