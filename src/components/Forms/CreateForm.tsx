ee5711d6746c30e6f8f77b9cb95bf3b78fa90fce
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-03 18:48:43 +0200

    update component CreateForm: add functional

--- src/components/Forms/CreateForm.tsx
+++ src/components/Forms/CreateForm.tsx
@@ -1,18 +1,42 @@
-import Input from "../UI/Input/Input.tsx";
-import Button from "../UI/Button/Button.tsx";
+import Button from '../UI/Button/Button.tsx';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { ChangeEvent, useState } from 'react';
+import { useMutation } from 'react-query';
+import { queryClient } from '../../Provider.tsx';
+import { createItem } from '../../api/request/items.ts';
 
 const CreateForm = () => {
-    return (
-        <form className={"form"}>
-            <Input placeholder={"Title"}/>
-            <Input placeholder={"Img"}/>
-            <Input placeholder={"Link"}/>
-            <Input placeholder="Category"/>
-            <div className={"form-btn"}>
-                <Button>Create</Button>
-            </div>
-        </form>
-    );
+  const { register, handleSubmit } = useForm();
+  const [imageBase64, setImageBase34] = useState<ImageType>('');
+  const handlerFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.length ? event.target.files[0] : null;
+    if (!file) return;
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setImageBase34(reader.result);
+    };
+
+    reader.readAsDataURL(file);
+  };
+  const mutation = useMutation((data: Item) => createItem({ params: data }), {
+    onSuccess: () => {
+      queryClient.invalidateQueries('items');
+    }
+  });
+  const onSubmit: SubmitHandler<Item> = (data) => mutation.mutate({ ...data, img: imageBase64 });
+
+  return (
+    <form className={'form'} onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('title')} placeholder={'Title'} />
+      <input {...register('img')} type={'file'} onChange={handlerFileChange} />
+      <input {...register('link')} placeholder={'Link'} />
+      <input {...register('category')} placeholder='Category' />
+      <div className={'form-btn'}>
+        <Button type={'submit'}>Create</Button>
+      </div>
+    </form>
+  );
 };
 
 export default CreateForm;
