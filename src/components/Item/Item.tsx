efab17991ba09702d21b03c866fb4b530724f3f5
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-03 18:52:05 +0200

    update component Item: all button Link

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,12 +1,17 @@
-import {FC} from "react";
+import { FC } from 'react';
 import Button from '../UI/Button/Button.tsx';
 import styles from './Item.module.css';
 
-const Item:FC<Item> = ({title}) => {
+const Item: FC<Item> = ({ title, link }) => {
   return (
     <section className={styles.wrapper}>
       <span className={styles.title}>{title}</span>
-      <Button>Open</Button>
+      <div className='form-btn'>
+        <Button>
+          <a href={link}>Link</a>
+        </Button>
+        <Button>Open</Button>
+      </div>
     </section>
   );
 };
