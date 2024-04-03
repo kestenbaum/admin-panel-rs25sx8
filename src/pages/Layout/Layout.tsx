f65cd143dd715be0580f051ab0d6b9f8cc810216
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-03 18:52:38 +0200

    update component Layout: delete forms

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,29 +1,24 @@
 import Item from '../../components/Item/Item.tsx';
-import CreateForm from "../../components/Forms/CreateForm.tsx";
-import UpdateForm from "../../components/Forms/UpdateForm.tsx";
 import styles from './Layout.module.css';
-import {useQuery} from "react-query";
-import {getItems} from "../../api/request/items.ts";
-import Loader from "../../components/UI/Loader/Loader.tsx";
+import { useQuery } from 'react-query';
+import { getItems } from '../../api/request/items.ts';
+import Loader from '../../components/UI/Loader/Loader.tsx';
 
 const Layout = () => {
-  const {data, isLoading} = useQuery({
-   queryKey: ["items"],
-   queryFn: () => getItems().then(responce => responce.data)
-  })
+  const { data, isLoading } = useQuery({
+    queryKey: ['items'],
+    queryFn: () => getItems().then((responce) => responce.data)
+  });
 
   return (
     <section className={styles.wrapper}>
       <div className='container'>
         <div className={styles.items}>
-          <CreateForm/>
-          <UpdateForm/>
-          {!isLoading
-              ?
-              data?.data.map(item => <Item key={item._id} title={item.title}/>)
-            :
-            <Loader/>
-          }
+          {!isLoading ? (
+            data?.data.map((item) => <Item key={item._id} title={item.title} link={item.link} />)
+          ) : (
+            <Loader />
+          )}
         </div>
       </div>
     </section>
