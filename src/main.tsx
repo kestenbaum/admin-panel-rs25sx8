cf67872fa56801992494fbf7753c5a96a5abfce4
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-03 18:53:30 +0200

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App.tsx';
-import {Provider} from "./Provider.tsx";
+import { Provider } from './Provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider>
-        <App />
-    </Provider>
-)
+  <Provider>
+    <App />
+  </Provider>
+);
