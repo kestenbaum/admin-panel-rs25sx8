fa307850215ed068a74a1493bbb7c939a81bade8
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-03 18:55:21 +0200

    update component Sidebar: add active function

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,22 @@
-import Button from "../UI/Button/Button.tsx";
-import {FC} from "react";
+import { FC } from 'react';
+import Button from '../UI/Button/Button.tsx';
+import styles from './Sidebar.module.css';
 
 interface SidebarProps {
-    active: boolean,
-    setActive: (active:boolean) => void
+  active: boolean;
+  setActive: (active: boolean) => void;
 }
 
-const Sidebar:FC<SidebarProps> = ({active, setActive}) => {
-    return (
-        <div className={!active ? "sidebar" : "sidebar active"}>
-            <Button onClick={() => setActive(false)}>Create</Button>
-            <Button onClick={() => setActive(false)}>LogOut</Button>
-        </div>
-    );
+const Sidebar: FC<SidebarProps> = ({ active, setActive }) => {
+  return (
+    <div
+      className={!active ? styles.sidebar : styles.sidebarActive}
+      onClick={() => setActive(false)}
+    >
+      <Button onClick={() => setActive(false)}>Create</Button>
+      <Button onClick={() => setActive(false)}>LogOut</Button>
+    </div>
+  );
 };
 
 export default Sidebar;
