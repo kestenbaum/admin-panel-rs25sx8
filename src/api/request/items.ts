bbead23715e2735308e36ec0ae4241985a40d22c
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-03 18:53:57 +0200

    update api:request<post>

--- src/api/request/items.ts
+++ src/api/request/items.ts
@@ -1,8 +1,16 @@
-import {api} from "../instance.ts";
+import { api } from '../instance.ts';
 
-export type GetWorksConfig = AxiosRequestConfig
+export type GetWorksConfig = AxiosRequestConfig;
 export const getItems = async (requestConfig?: GetWorksConfig) =>
-    api.get<GetItems>('/works', requestConfig?.config)
+  api.get<GetItems>('/works', requestConfig?.config);
 
+export type PostItemParams = Omit<Item, '_id'>;
+export type PostItemConfig = AxiosRequestConfig<PostItemParams>;
 
-
+export const createItem = async ({
+  params,
+  config
+}: {
+  params: PostItemParams;
+  config?: PostItemConfig;
+}) => api.post<Item>('/works', params, config);
