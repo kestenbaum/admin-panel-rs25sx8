11d651907230c74ef4a73512e7c76c1e005a330c
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-07 14:05:16 +0200

    update component UpdateForm: add props and delete input:category

--- src/components/Forms/UpdateForm.tsx
+++ src/components/Forms/UpdateForm.tsx
@@ -1,12 +1,13 @@
+import {FC} from "react";
 import Button from '../UI/Button/Button.tsx';
 
-const UpdateForm = () => {
+const UpdateForm:FC<Item> = ({title, link}) => {
+
   return (
     <form className={'form'}>
-      <input placeholder={'Title'} />
-      <input placeholder={'Img'} />
-      <input placeholder={'Link'} />
-      <input placeholder='Category' />
+      <input placeholder={title} />
+      <input placeholder={"img"} />
+      <input placeholder={link} />
       <div className={'form-btn'}>
         <Button>Delete</Button>
         <Button>Update</Button>
