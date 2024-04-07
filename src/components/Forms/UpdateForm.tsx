176495b32d77a1ce890742dff3bff069280fcaa1
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-07 14:05:54 +0200

    update component: prettierrc

--- src/components/Forms/UpdateForm.tsx
+++ src/components/Forms/UpdateForm.tsx
@@ -1,12 +1,11 @@
-import {FC} from "react";
+import { FC } from 'react';
 import Button from '../UI/Button/Button.tsx';
 
-const UpdateForm:FC<Item> = ({title, link}) => {
-
+const UpdateForm: FC<Item> = ({ title, link }) => {
   return (
     <form className={'form'}>
       <input placeholder={title} />
-      <input placeholder={"img"} />
+      <input placeholder={'img'} />
       <input placeholder={link} />
       <div className={'form-btn'}>
         <Button>Delete</Button>
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,26 +1,26 @@
-import {FC, useState} from 'react';
+import { FC, useState } from 'react';
 import Button from '../UI/Button/Button.tsx';
 import PopupDefault from '../UI/Popup/PopupDefault.tsx';
 import UpdateForm from '../Forms/UpdateForm.tsx';
 import styles from './Item.module.css';
 
 const Item: FC<Item> = ({ title, link }) => {
-    const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
   return (
-      <>
-          <PopupDefault showModal={open} setShowModal={setOpen}>
-              <UpdateForm title={title} link={link}/>
-          </PopupDefault>
-          <section className={styles.wrapper}>
-              <span className={styles.title}>{title}</span>
-              <div className='form-btn'>
-                  <Button>
-                      <a href={link}>Link</a>
-                  </Button>
-                  <Button onClick={() => setOpen(!open)}>Open</Button>
-              </div>
-          </section>
-      </>
+    <>
+      <PopupDefault showModal={open} setShowModal={setOpen}>
+        <UpdateForm title={title} link={link} />
+      </PopupDefault>
+      <section className={styles.wrapper}>
+        <span className={styles.title}>{title}</span>
+        <div className='form-btn'>
+          <Button>
+            <a href={link}>Link</a>
+          </Button>
+          <Button onClick={() => setOpen(!open)}>Open</Button>
+        </div>
+      </section>
+    </>
   );
 };
 
