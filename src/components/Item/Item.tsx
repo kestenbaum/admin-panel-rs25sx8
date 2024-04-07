a4899b412ea8be56d85e5ed9d34a0edc7e325a9b
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-07 14:04:06 +0200

    update component Item: add Popup

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,18 +1,26 @@
-import { FC } from 'react';
+import {FC, useState} from 'react';
 import Button from '../UI/Button/Button.tsx';
+import PopupDefault from '../UI/Popup/PopupDefault.tsx';
+import UpdateForm from '../Forms/UpdateForm.tsx';
 import styles from './Item.module.css';
 
 const Item: FC<Item> = ({ title, link }) => {
+    const [open, setOpen] = useState<boolean>(false);
   return (
-    <section className={styles.wrapper}>
-      <span className={styles.title}>{title}</span>
-      <div className='form-btn'>
-        <Button>
-          <a href={link}>Link</a>
-        </Button>
-        <Button>Open</Button>
-      </div>
-    </section>
+      <>
+          <PopupDefault showModal={open} setShowModal={setOpen}>
+              <UpdateForm title={title} link={link}/>
+          </PopupDefault>
+          <section className={styles.wrapper}>
+              <span className={styles.title}>{title}</span>
+              <div className='form-btn'>
+                  <Button>
+                      <a href={link}>Link</a>
+                  </Button>
+                  <Button onClick={() => setOpen(!open)}>Open</Button>
+              </div>
+          </section>
+      </>
   );
 };
 
