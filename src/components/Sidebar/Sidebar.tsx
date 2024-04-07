538f19de868f190d89a35cf31f96d3f45b6afe20
Oleksii Vovnenko <kespa71for@gmail.com>
2024-04-07 13:50:02 +0200

    add component Popup

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Button from '../UI/Button/Button.tsx';
+import PopupDefault from '../UI/Popup/PopupDefault.tsx';
 import styles from './Sidebar.module.css';
+import CreateForm from '../Forms/CreateForm.tsx';
 
 interface SidebarProps {
   active: boolean;
@@ -8,14 +10,28 @@ interface SidebarProps {
 }
 
 const Sidebar: FC<SidebarProps> = ({ active, setActive }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <div
-      className={!active ? styles.sidebar : styles.sidebarActive}
-      onClick={() => setActive(false)}
-    >
-      <Button onClick={() => setActive(false)}>Create</Button>
-      <Button onClick={() => setActive(false)}>LogOut</Button>
-    </div>
+    <>
+      <PopupDefault showModal={open} setShowModal={setOpen}>
+        <CreateForm />
+      </PopupDefault>
+      <div
+        className={!active ? styles.sidebar : styles.sidebarActive}
+        onClick={() => setActive(false)}
+      >
+        <Button
+          onClick={() => {
+            setOpen(!open);
+            setActive(false);
+          }}
+        >
+          Create
+        </Button>
+        <Button onClick={() => setActive(false)}>LogOut</Button>
+      </div>
+    </>
   );
 };
 
